96de65be4fbc059c0eb697d61553e3d5cfa7b9b3
htetko <htetko.connetis@gmail.com>
2022-11-07 20:33:31 +0630

    Modal Box component create

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ModalBox />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ModalBox from './components/ModalBox.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ModalBox,
   }
 }
 </script>
---
+++ src/components/ModalBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="modalBox">
+    <h1>Modal box</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+ .modalBox {
+    border: 1px solid red;
+    width: 400px;
+    height: 300px;
+    background: darkred;
+    margin: 10px auto;
+}
+.modalBox h1 {
+    color: #fff;
+}
+
+</style>
